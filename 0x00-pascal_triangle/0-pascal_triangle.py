a1761e373f4ae81939c36c605957b0511905aab8
iamMAHAM <abdulkabore@gmail.com>
2022-12-22 09:06:15 +0000

    comment function

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
+"""
+a script that builds a pascal triangle
+"""
+
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-
+    """function that build a pascal triangle"""
     triangle = []
 
-    for i in range(n):
-        row = []
-        if (i + 1 >= 2):
+    if (n >= 0):
+        for i in range(n):
+            row = []
+            if (i + 1 >= 2):
+                row.append(1)
+                for j in range(1, i):
+                    previous = triangle[i - 1]
+                    row.append(previous[j - 1] + previous[j])
             row.append(1)
-            for j in range(1, i):
-                previous = triangle[i - 1]
-                row.append(previous[j - 1] + previous[j])
-        row.append(1)
-        triangle.append(row)
+            triangle.append(row)
 
     return triangle

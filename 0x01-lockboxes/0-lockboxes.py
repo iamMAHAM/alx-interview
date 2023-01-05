daef2cfa452f5eaddb259a00acce0acd6356aa13
iamMAHAM <abdulkabore@gmail.com>
2023-01-05 10:32:27 +0000

    document code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,6 +5,9 @@ Lockbox - all box
 
 
 def canUnlockAll(boxes):
+    """
+        checks if all boxes can be unlocked
+    """
     unlocked_boxes = [False] * len(boxes)  # set unlock all boxes to False
     unlocked_boxes[0] = True  # first box is always unlocked
     taskes = [0]
@@ -13,6 +16,6 @@ def canUnlockAll(boxes):
         boxe = taskes.pop()  # get a next boxe
         for key in boxes[boxe]:  # for all key insides current box
             if not unlocked_boxes[key]:  # if box is not unlocked yet
-                unlocked_boxes[key] = True
-                taskes.append(key)
+                unlocked_boxes[key] = True  # set box to unlocked
+                taskes.append(key)  # append unlocked keys to taskes
     return all(unlocked_boxes)  # like a every method in javascript

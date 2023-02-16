1b2421c8912c99e536bdaa4c0f1ca8a102cea5bf
iamMAHAM <abdulkabore@gmail.com>
2023-02-16 17:32:04 +0000

    rotates 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List[int]]):
+    """
+    rotates 2d matrix
+    """
+
+    N: int = len(matrix)
+
+    matrix_copy: List[int] = []
+    copy_row: int = 0
+    for column in range(N):
+        for row in range((N - 1), -1, -1):
+            if column is 0:
+                matrix_copy.append([])
+            matrix_copy[copy_row].append(matrix[row][column])
+        copy_row += 1
+
+    for row in range(N):
+        for column in range(N):
+            matrix[row][column] = matrix_copy[row][column]
